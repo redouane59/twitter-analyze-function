be5e555229e4566ff237e7aa7493345f0d2a18ba
Redouane59 <bali.redouane@gmail.com>
2021-08-06 18:36:02 +0200

    divizio01

--- src/test/java/LoadMissingFollowersTest.java
+++ src/test/java/LoadMissingFollowersTest.java
@@ -1,13 +1,17 @@
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.Redouane59.twitter.model.FollowersAnalyzer;
+import io.github.Redouane59.twitter.model.InfluentUser;
 import io.github.redouane59.twitter.TwitterClient;
 import io.github.redouane59.twitter.dto.user.User;
+import io.github.redouane59.twitter.dto.user.UserV2;
 import io.github.redouane59.twitter.signature.TwitterCredentials;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
@@ -22,11 +26,12 @@ public class LoadMissingFollowersTest {
   }
 
   @Test
+  @Disabled
   public void loadMissingFollowers() {
 
     ObjectMapper mapper = new ObjectMapper();
 
-    List<String> userNames = List.of("BHL");
+    List<String> userNames = List.of("DIVIZIO1");
     System.out.println("\n*** STARTING LOADING FOLLOWERS ***\n");
     for (String userName : userNames) {
       try {
@@ -40,6 +45,46 @@ public class LoadMissingFollowersTest {
       } catch (Exception e) {
         LOGGER.error(e.getMessage());
       }
+    }
+  }
+
+  // @todo add test to check if the follower count is OK
+  @Test
+  public void testInfluentUserCount() {
+    FollowersAnalyzer followersAnalyzer = new FollowersAnalyzer(twitterClient);
+    System.out.println("");
+    for (InfluentUser influentUser : followersAnalyzer.INFLUENT_USERS) {
+      boolean reliable = isFollowersIdsDataReliable(influentUser);
+    }
+    System.out.println("");
+  }
+
+  private boolean isFollowersIdsDataReliable(InfluentUser influencer) {
+    int offlineFollowersCount = influencer.getFollowerIds().size();
+    if (offlineFollowersCount == 0) {
+      LOGGER.error("No offline followers found for " + influencer.getName());
+      return false;
+    }
+    User user = twitterClient.getUserFromUserName(influencer.getName());
+    if (user == null || ((UserV2) user).getData() == null) {
+      LOGGER.error(influencer.getName() + " not found !");
+      return false;
+    }
+    int    apiFollowersCount = user.getFollowersCount();
+    int    diff              = Math.abs(apiFollowersCount - offlineFollowersCount);
+    double acceptableDelta   = 0.1;
+    if (diff > acceptableDelta * apiFollowersCount) {
+      LOGGER.error("/!\\ Follower ids data doesn't look accurate for "
+                   + influencer.getName()
+                   + " ("
+                   + offlineFollowersCount
+                   + " ids VS "
+                   + apiFollowersCount
+                   + " followers ");
+      return false;
+    } else {
+      LOGGER.debug("followers ids OK for " + influencer.getName());
+      return true;
     }
   }
 
